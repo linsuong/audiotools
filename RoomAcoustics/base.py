afeaf656aaace57ddd7068724d0064b795d22e1c
Linus <linusong22@gmail.com>
2024-02-15 17:42:16 +0000

    add ideas for personal projects

---
+++ RoomAcoustics/base.py
@@ -0,0 +1,28 @@
+import numpy as np
+from matplotlib.pyplot as plt
+
+class Room(self, size, reflectorType):
+    """
+    generates a 4 sided structure given specific dimensions
+
+    Args:
+        size (list): breadth, width, height of a room.
+        reflectorType (str): specifies the type of reflector.
+            perfect - a perfect reflector, i.e. a flat wall.
+            treated - a treated room with acoustic panels.
+            rough - a wall that is slightly rough.
+    """
+
+    b, l, w = size
+
+    
+class MicrophoneSource(self, x, y, z):
+
+def imageSource():
+    return
+
+def rayTracing():
+    return None
+
+def 
+
